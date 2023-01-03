26099288afe235ba369d084856714e4f3a416290
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 13:56:21 +0100

    fix: handle forbidden error

---
+++ errors/forbidden.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from './customAPIError.js';
+
+class ForbiddenError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+
+    this.status = 'fail';
+    this.statusCode = StatusCodes.FORBIDDEN;
+  };
+};
+
+export default ForbiddenError;
