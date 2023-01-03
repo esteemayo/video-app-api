7d0315d17d318b24ed7c61dbdedce05713de67da
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 02:44:33 +0100

    fix: import all the routes

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
+// requiring routes
+import authRoute from './routes/auth.js';
+import userRoute from './routes/users.js';
+import videoRoute from './routes/videos.js';
+import commentRoute from './routes/comments.js';
+
 dotenv.config({ path: './variables.env' });
 
 const app = express();
