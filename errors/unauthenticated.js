1eeac3515ee6566d74971370a2ea1f833dff9c68
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 13:56:54 +0100

    fix: handle unauthorized errors

---
+++ errors/unauthenticated.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from './customAPIError.js';
+
+class UnauthenticatedError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+
+    this.status = 'fail';
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  };
+};
+
+export default UnauthenticatedError;
