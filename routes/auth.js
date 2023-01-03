609b62951add677b6138283ed4fce59468e1a424
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 03:12:21 +0100

    feat: create user' sign up endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import * as authController from '../controllers/authController.js';
+
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 export default router;
