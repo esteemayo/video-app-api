977f381595486ffbc40d1b713cd2d23bceca8848
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 14:46:51 +0100

    fix: import BadRequestError error class

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
 import UnauthenticatedError from './../errors/unauthenticated.js';
+import BadRequestError from './../errors/badRequest.js';
 
 export const signup = asyncHandler(async (req, res, next) => {
   const user = await User.create({ ...req.body });
