98b392cfd49e22c05e7baaf63e106403798b9412
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 03:02:11 +0100

    fix: disable eslint

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,15 @@
+/* eslint-disable */
 import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
+
+export const register = asyncHandler(async (req, res, next) => { });
+
+export const login = asyncHandler(async (req, res, next) => { });
+
+export const forgotPassword = asyncHandler(async (req, res, next) => { });
+
+export const resetPassword = asyncHandler(async (req, res, next) => { });
+
+export const updatePassword = asyncHandler(async (req, res, next) => { });
