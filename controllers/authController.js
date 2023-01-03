9503d3526bee0da494ee786eac649fe7ab1919a9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 22:20:00 +0100

    feat: import sendEmail function

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,7 @@ import UnauthenticatedError from './../errors/unauthenticated.js';
 import BadRequestError from './../errors/badRequest.js';
 import NotFoundError from '../errors/notFound.js';
 import createSendToken from '../utils/createSendToken.js';
+import sendEmail from './../utils/email.js';
 
 export const signup = asyncHandler(async (req, res, next) => {
   const user = await User.create({ ...req.body });
