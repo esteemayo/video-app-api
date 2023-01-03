cd40fd0f43bf16011c2935541a24fcca01471f56
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 16:01:50 +0100

    feat: add all users' route endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,32 @@
 import express from 'express';
 
+import * as userController from '../controllers/userController.js';
+
 const router = express.Router();
+
+router.patch('/update-me', userController.updateMe);
+
+router.get('/me', userController.getMe, userController.getUser);
+
+router.delete('/delete-me', userController.deleteMe);
+
+router.patch('/subscribe/:id', userController.subscribe);
+
+router.patch('/unsubscibe/:id', userController.unsubscribe);
+
+router.patch('/like/:videoId', userController.likeVideo);
+
+router.patch('/dislike/:videoId', userController.dislikeVideo);
+
+router
+  .route('/')
+  .get(userController.getUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 export default router;
