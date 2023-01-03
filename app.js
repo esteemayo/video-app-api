d03ca882eb56b1fc976f1fd87791e655674ba431
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 02:44:58 +0100

    feat: define each route endpoints

--- app.js
+++ app.js
@@ -16,4 +16,10 @@ if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
 
+// api routes
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/videos', videoRoute);
+app.use('/api/v1/comments', commentRoute);
+
 export default app;
