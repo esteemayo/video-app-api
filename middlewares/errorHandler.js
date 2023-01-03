7db3a6393a05ae58ae8490584d48be3e6b2633cc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 14:08:14 +0100

    feat: handle app global error handling

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+const globalErrorHandler = (err, req, res, next) => {
+  const customError = {
+    status: err.status || 'error',
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went very wrong',
+    stack: err.stack,
+  };
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(customError, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(customError, res);
+  }
+};
+
+export default globalErrorHandler;
