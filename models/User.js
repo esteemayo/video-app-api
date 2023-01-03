26da2603fc2891b38c5aa2c88edd9c9053e1ef33
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 22:03:30 +0100

    feat: create createPasswordResetToken function

--- models/User.js
+++ models/User.js
@@ -103,6 +103,19 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   return false;
 };
 
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
