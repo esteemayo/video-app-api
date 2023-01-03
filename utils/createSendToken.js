59820423b5059acf06f57cca3871e930b98c9bcf
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 21:53:42 +0100

    fix: refactor

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -1,22 +1,20 @@
 /* eslint-disable */
-
 const createSendToken = (user, status, req, res) => {
   const token = user.generateToken();
 
+  res.cookie('access_token', token, {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
+
   const { password, ...rest } = user._doc;
 
-  res
-    .cookie('access_token', token, {
-      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-      sameSite: true,
-      secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    })
-    .status(status)
-    .json({
-      status: 'success',
-      ...rest,
-    });
+  res.status(status).json({
+    status: 'success',
+    ...rest,
+  });
 };
 
 export default createSendToken;
