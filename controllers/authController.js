4acdb906d8c43ad1e94f6c1dad2e8f6c7430b366
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 22:27:08 +0100

    fix: handle forgot user' password logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,7 +33,65 @@ export const signin = asyncHandler(async (req, res, next) => {
   createSendToken(user, StatusCodes.OK, req, res);
 });
 
-export const forgotPassword = asyncHandler(async (req, res, next) => { });
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return next(new BadRequestError('Please enter your email address'));
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(new NotFoundError('There is no user with the email address'));
+  }
+
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/reset-password/${resetToken}`;
+
+  const message = `
+    Hi ${user.name},
+    There was a request to change your password!
+    If you did not make this request then please ignore this email.
+    Otherwise, please click this link to change your password: ${resetURL}
+  `;
+
+  const html = `
+    <div style='background: #f7f7f7; color: #333; padding: 50px; text-align: left;'>
+      <h3>Hi ${user.name},</h3>
+      <p>There was a request to change your password!</p>
+      <p>If you did not make this request then please ignore this email.</p>
+      <p>Otherwise, please click this link to change your password: 
+        <a href='${resetURL}'>Reset my password →</a>
+      </p>
+    </div>
+  `;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 mins)',
+      message,
+      html,
+    });
+
+    res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: 'Token sent to email',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new CustomAPIError('There was an error sending the email. Please try again later')
+    );
+  }
+});
 
 export const resetPassword = asyncHandler(async (req, res, next) => { });
 
