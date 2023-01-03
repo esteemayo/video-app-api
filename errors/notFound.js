5af1a5855dc3f1a09e860cecef11e3eba03b6180
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 13:56:38 +0100

    fix: handle not found errors

---
+++ errors/notFound.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from './customAPIError.js';
+
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+
+    this.status = 'fail';
+    this.statusCode = StatusCodes.NOT_FOUND;
+  };
+};
+
+export default NotFoundError;
