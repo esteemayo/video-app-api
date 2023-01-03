48f974f541ed0e65b2d79df0d4b4e74f5b487f4d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-03 13:56:04 +0100

    fix: handle bad request error

---
+++ errors/badRequest.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from './customAPIError.js';
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+
+    this.status = 'fail';
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  };
+};
+
+export default BadRequestError;
