b838963da813f75b17efe5d3105d29c0bc3a3e76
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-04 14:31:13 +0100

    fix: add create new video logic

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -11,7 +11,16 @@ export const getVideoById = asyncHandler(async (req, res, next) => { });
 
 export const getVideoBySlug = asyncHandler(async (req, res, next) => { });
 
-export const createVideo = asyncHandler(async (req, res, next) => { });
+export const createVideo = asyncHandler(async (req, res, next) => {
+  if (!req.body.user) req.body.user = req.user.id;
+
+  const video = await Video.create({ ...req.body });
+
+  res.status(StatusCodes.CREATED).json({
+    status: 'success',
+    video,
+  });
+});
 
 export const updateVideo = asyncHandler(async (req, res, next) => { });
 
