dd97b84cd0305f34cd3b5a9ed4b875a0eb20d7fb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-04 01:39:36 +0100

    feat: import authMiddleware

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import express from 'express';
 
+import * as authMiddleware from '../middlewares/authMiddleware.js';
 import * as videoController from '../controllers/videoController.js';
 
 const router = express.Router();
