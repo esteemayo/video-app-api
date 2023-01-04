1da1e21c38633307119208acc7bf20cdf3c43e57
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-04 01:04:13 +0100

    feat: add xss middleware

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ app.use(express.json({ limit: '10kb' }));
 
 app.use(cookieParser());
 
+app.use(xss());
+
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
