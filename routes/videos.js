0c0fc272fcd0e144b6121ceb62f9d4f462ed31ca
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-04 01:47:56 +0100

    fix: define all video endpoints

--- routes/videos.js
+++ routes/videos.js
@@ -6,4 +6,27 @@ import * as videoController from '../controllers/videoController.js';
 
 const router = express.Router();
 
+router.get(
+  '/details/:slug',
+  authMiddleware.verifyUser,
+  videoController.getVideoBySlug,
+);
+
+router
+  .route('/')
+  .get(videoController.getVideos)
+  .post(
+    authMiddleware.protect,
+    authMiddleware.verifyUser,
+    videoController.createVideo,
+  );
+
+router.use(authMiddleware.protect);
+
+router
+  .route('/:id')
+  .get(authMiddleware.verifyUser, videoController.getVideoById)
+  .patch(authMiddleware.verifyUser, videoController.updateVideo)
+  .delete(authMiddleware.verifyUser, videoController.deleteVideo);
+
 export default router;
