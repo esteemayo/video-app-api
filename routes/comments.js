d49700c4313cbc93eb3121f0fff63b1abc3bb986
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 20:09:12 +0100

    feat: add commentController

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import * as authMiddleware from '../middlewares/authMiddleware.js';
+import * as commentController from '../controllers/commentController.js';
 
 const router = express.Router();
 
