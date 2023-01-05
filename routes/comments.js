aa8a2a018b7ac0737ae4e0642635b59a36de860f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 20:11:25 +0100

    fix: define all comment endpoints

--- routes/comments.js
+++ routes/comments.js
@@ -6,4 +6,15 @@ import * as commentController from '../controllers/commentController.js';
 
 const router = express.Router();
 
+router
+  .route('/')
+  .get(commentController.getComments)
+  .post(commentController.createComment);
+
+router
+  .route('/:id')
+  .get(commentController.getComment)
+  .patch(commentController.updateComment)
+  .delete(commentController.deleteComment);
+
 export default router;
