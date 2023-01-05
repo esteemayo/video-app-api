45d810b06e7279f945934d5b3d4cda122e3fad33
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 19:58:22 +0100

    feat: modify

---
+++ controllers/commentController.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import { StatusCodes } from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+
+import Comment from './../models/Comment.js';
+
+export const getComments = asyncHandler(async (req, res, next) => { });
+
+export const getComment = asyncHandler(async (req, res, next) => { });
+
+export const createComment = asyncHandler(async (req, res, next) => { });
+
+export const updateComment = asyncHandler(async (req, res, next) => { });
+
+export const deleteComment = asyncHandler(async (req, res, next) => { });
