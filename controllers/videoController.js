cea64f665dea77c1c42f1158ba8b7d5ead82dd2e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 17:44:18 +0100

    fix: fetch videos by tags

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -53,7 +53,16 @@ export const subscribe = asyncHandler(async (req, res, next) => {
   });
 });
 
-export const getVideosByTag = asyncHandler(async (req, res, next) => { });
+export const getVideosByTag = asyncHandler(async (req, res, next) => {
+  const tags = req.query.tags.split(',');
+
+  const videos = await Video.find({ tags: { $in: tags } });
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    videos,
+  });
+});
 
 export const searchVideo = asyncHandler(async (req, res, next) => { });
 
