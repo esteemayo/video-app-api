283a1e30937b60a80895c85bcaf0217b19542b13
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 15:45:12 +0100

    fix: modify

--- models/Video.js
+++ models/Video.js
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 
 const videoSchema = new mongoose.Schema({
-  // userId: {
-  //   type: String,
-  //   required: true,
-  // },
   title: {
     type: String,
     required: true,
