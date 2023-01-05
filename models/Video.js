8aedfb32bad6071c9ec1c91592671cc35ed0d0c3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 15:55:36 +0100

    fix: handle video' slug

--- models/Video.js
+++ models/Video.js
@@ -46,6 +46,18 @@ const videoSchema = new mongoose.Schema({
   timestamps: true,
 });
 
+videoSchema.pre('save', async function (next) {
+  if (!this.isModified('title')) return next();
+  this.slug = slugify(this.title, { lower: true });
+
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const videoWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+  if (videoWithSlug.length) {
+    this.slug = `${this.slug}-${videoWithSlug.length + 1}`;
+  }
+});
+
 const Video = mongoose.models.Video || mongoose.model('Video', videoSchema);
 
 export default Video;
