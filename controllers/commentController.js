6182d3289e1436d5bcea771f57053d140f02b1bd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 20:04:42 +0100

    fix: fetch all comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,8 +3,24 @@ import { StatusCodes } from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 
 import Comment from './../models/Comment.js';
+import APIFeatures from '../utils/apiFeatures.js';
 
-export const getComments = asyncHandler(async (req, res, next) => { });
+export const getComments = asyncHandler(async (req, res, next) => {
+  const features = new APIFeatures(Comment.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const comments = await features.query;
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    nbHits: comments.length,
+    comments,
+  });
+});
 
 export const getComment = asyncHandler(async (req, res, next) => { });
 
