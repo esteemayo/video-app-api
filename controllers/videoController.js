f9574f4cf18e9bcecdf184302b9e9d6c2f5d59a7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 19:08:50 +0100

    fix: refactor

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,36 +10,13 @@ import ForbiddenError from '../errors/forbidden.js';
 import NotFoundError from '../errors/notFound.js';
 
 export const getVideos = asyncHandler(async (req, res, next) => {
-  const queryObj = { ...req.query };
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((item) => delete queryObj[item]);
+  const features = new APIFeatures(Video.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-  let query = Video.find(JSON.parse(queryStr));
-
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
-
-  if (req.query.fields) {
-    const fields = req.query.fields.split(',').join(' ');
-    query = query.select(fields);
-  } else {
-    query = query.select('-__v');
-  }
-
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 100;
-  const skip = (page - 1) * limit;
-
-  query = query.skip(skip).limit(limit);
-
-  const videos = await query;
+  const videos = await features.query;
 
   res.status(StatusCodes.OK).json({
     status: 'success',
