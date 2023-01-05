3f78234c071d5818440b240563810e87ce6b1073
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-05 18:52:36 +0100

    fix: handle pagination

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -34,6 +34,12 @@ export const getVideos = asyncHandler(async (req, res, next) => {
     query = query.select('-__v');
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 100;
+  const skip = (page - 1) * limit;
+
+  query = query.skip(skip).limit(limit);
+
   const videos = await query;
 
   res.status(StatusCodes.OK).json({
