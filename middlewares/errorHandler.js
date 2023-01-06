e5e4465fcfa9d3215b3847e5c18a3f8e7f4daed8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-06 22:36:33 +0100

    feat: modify

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -28,20 +28,18 @@ const handleJWTExpiredError = (customError) => {
   customError.statusCode = StatusCodes.UNAUTHORIZED;
 };
 
-const sendErrorDev = (err, res) => {
+const sendErrorDev = (err, res) =>
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
     stack: err.stack,
   });
-};
 
-const sendErrorProd = (err, res) => {
+const sendErrorProd = (err, res) =>
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
-};
 
 const globalErrorHandler = (err, req, res, next) => {
   const customError = {
