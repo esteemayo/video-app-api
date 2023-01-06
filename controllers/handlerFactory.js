44b6c709b690aef1f1c8517913e173a9108eba12
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-06 21:58:20 +0100

    fix: add a hack to allow for nested get request of comments on video

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -7,7 +7,10 @@ import NotFoundError from '../errors/notFound.js';
 
 export const getAll = (Model) =>
   asyncHandler(async (req, res, next) => {
-    const features = new APIFeatures(Model.find(), req.query)
+    let filter = {};
+    if (req.params.videoId) filter = { video: req.params.videoId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
