e739bacd31cfcebb44db34183810fcfdb4d903e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-06 22:23:29 +0100

    fix: handle mongodb validation error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -11,6 +11,13 @@ const handleDuplicateFieldsDB = (customError, err) => {
   customError.statusCode = StatusCodes.BAD_REQUEST;
 };
 
+const handleValidationErrorDB = (customError, err) => {
+  customError.message = Object.values(err.errors)
+    .map((el) => el.message)
+    .join(', ');
+  customError.statusCode = StatusCodes.BAD_REQUEST;
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -36,6 +43,7 @@ const globalErrorHandler = (err, req, res, next) => {
 
   if (err.name === 'CastError') handleCastErrorDB(customError, err);
   if (err.code && err.code === 11000) handleDuplicateFieldsDB(customError, err);
+  if (err.name === 'ValidationError') handleValidationErrorDB(customError, err);
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(customError, res);
