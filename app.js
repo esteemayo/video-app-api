84c8896b7899e824d62207126fa181e46d751f07
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-11 14:45:32 +0100

    fix: change rate limit max to 1000

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (app.get('env') === 'development') {
 }
 
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, Please try again in an hour',
 });
