49d4f696ffe5c40105009a9ae6c972f6858bd0b4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-12 01:07:28 +0100

    feat: refactor google response

---
+++ utils/createSendGoogleToken.js
@@ -0,0 +1,18 @@
+const createSendGoogleToken = (user, status, req, res) => {
+  const token = user.generateToken();
+
+  res.cookie('access_token', token, {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
+
+  res.status(status).json({
+    status: 'success',
+    token,
+    user: user._doc,
+  });
+};
+
+export default createSendGoogleToken;
