745e010cbe07a384de5dc7cb4bce4b2026a16d4d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-15 04:12:11 +0100

    fix: format

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -3,7 +3,9 @@ const createSendToken = (user, status, req, res) => {
   const token = user.generateToken();
 
   res.cookie('access_token', token, {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
     sameSite: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
