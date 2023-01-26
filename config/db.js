fe0a1d32e36fd948dd536d303dff6d5034183789
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-26 19:38:06 +0100

    fix: modify

--- config/db.js
+++ config/db.js
@@ -21,7 +21,7 @@ const connectDB = async () => {
 mongoose.set('strictQuery', false);
 
 mongoose.connection.on('disconnected', () => {
-  console.log('MongoDB disconnected'.red.bold);
+  console.log('MongoDB disconnected'.strikethrough);
 });
 
 export default connectDB;
