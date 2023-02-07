8ea22a289ccdcb1b9809294faec2a05d7c464d73
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-07 14:44:50 +0100

    fix: format code

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
-import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
 import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
+import mongoose, { Schema } from 'mongoose';
 
 const userSchema = new Schema({
   name: {
